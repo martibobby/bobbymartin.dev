fe82c0b33c2f059fd5c61bfe5c6b9c7fd855129f
Bobby Martin <martibobby@gmail.com>
2024-05-03 00:14:25 -0500

    consolidate npm packages, use 4321 docker port to mimic astro local dev, cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml';
 
-import relativeLinks from "astro-relative-links";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), relativeLinks()],
+  integrations: [tailwind()],
   base: './',
+  site: 'https://www.bobbymartin.dev',
   vite: {
     plugins: [yaml()]
   }
