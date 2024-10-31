5bcdc45bf517a2fa83df514fdd766e31779aff6a
Bobby Martin <martibobby@gmail.com>
2024-10-30 22:33:48 -0500

    change resume pdf output filename

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -11,7 +11,7 @@ async function generatePDF(url: string) {
     // Ignore 'print' CSS media type - use regular CSS types
     // await page.emulateMediaType('screen');
     // Generate PDF and save it
-    await page.pdf({ path: 'example.pdf', format: 'letter' });
+    await page.pdf({ path: 'bobby_martin_resume.pdf', format: 'letter' });
     // Close the browser
     await browser.close();
 }
