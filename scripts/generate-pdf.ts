a58b1b442fc35757b93cd65725754f4181987a06
Bobby Martin <martibobby@gmail.com>
2025-05-29 00:06:17 -0500

    pdf export: ignore default chromium security sandbox for github actions

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -2,14 +2,13 @@
 import puppeteer from 'puppeteer';
 
 async function generatePDF(url: string) {
-    // Launch a new headless browser instance
-    const browser = await puppeteer.launch();
-    // const browser = await puppeteer.launch({
-    //     args: [
-    //         '--no-sandbox',
-    //         '--disable-setuid-sandbox'
-    //     ]
-    // });
+    // Launch a new headless browser instance (no sandbox for container in github actions)
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
     // Create a new page
     const page = await browser.newPage();
     // Go to a webpage
